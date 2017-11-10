a703ab1f0a3a3043d6f04a209a3c84d20bf9fa96
Leslie <jinl0ng@icloud.com>
2017-11-10 14:59:03 +0800

    add Recursive method

--- offer12.cpp
+++ offer12.cpp
@@ -39,9 +39,21 @@ bool Increasement(std::string & str, int n)
 void print(std::string str)
 {
     int index = 0;
-    while(str[index]=='0' && str.size()>1)
-        ++index;
-    std::cout << std::string(str, index) << std::endl;
+    bool isAllZero = true;
+    for(size_t i=0; i<str.size(); ++i)
+        if(str[i]!='0')
+        {
+            isAllZero = false;
+            break;
+        }
+    if(isAllZero)
+        std::cout << '0' << std::endl;
+    else 
+    {
+        while(str[index]=='0' && str.size()>1)
+            ++index;
+        std::cout << std::string(str, index) << std::endl;
+    }
 }
 
 
@@ -58,8 +70,40 @@ void PrintNumber_Iteratively(int n)
 }
 
 
+void PrintNumber_Recursively_Core(std::string & str, size_t index)
+{
+    if(index==str.size())
+    {
+        print(str);
+        return;
+    }
+
+    for(int i=0; i<=9; ++i)
+    {
+        str[index] = '0' + i;
+        PrintNumber_Recursively_Core(str, index+1);
+    }
+}
+
+
+void PrintNumber_Recursively(int n)
+{
+    assert(n>=1);
+
+    std::string str(n, '0');
+    for(int i=0; i<=9; ++i)
+    {
+        str[0] = '0' + i;
+        PrintNumber_Recursively_Core(str, 1);
+    }
+}
+
+
 int main()
 {
-    PrintNumber_Iteratively(7);
+    int n = 2;
+    PrintNumber_Recursively(n);
+    std::cout << "Iteratively : " << std::endl;
+    PrintNumber_Iteratively(n);
     return 0;
 }
