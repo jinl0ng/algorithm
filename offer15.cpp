08cca037b6b4724350635837917c66ecc2e676f7
Leslie <jinl0ng@icloud.com>
2017-11-18 12:48:42 +0800

    finished

---
+++ offer15.cpp
@@ -0,0 +1,45 @@
+#include "List.h"
+#include <iostream>
+#include <assert.h>
+
+
+ListNode* LastKNode(ListNode** pHead, unsigned k) {
+  assert(pHead!=nullptr&&*pHead!=nullptr&&k>0);
+
+  unsigned count = 1;
+  ListNode* ahead = *pHead;
+  ListNode* behind = nullptr;
+  for (; count<k&&ahead!=nullptr;++count) 
+    ahead = ahead->m_pNext;
+  if (count==k&&ahead!=nullptr) {
+    for (behind=*pHead; ahead->m_pNext!=nullptr; ahead=ahead->m_pNext)
+      behind = behind->m_pNext;
+    return behind;
+  } else {
+    return nullptr;
+  }
+}
+
+
+int main() {
+  ListNode* pTmp = nullptr;
+  ListNode** test = &pTmp;
+  AddToTail(test, 1);
+  AddToTail(test, 2);
+  AddToTail(test, 3);
+  AddToTail(test, 4);
+  AddToTail(test, 5);
+  AddToTail(test, 6);
+  AddToTail(test, 7);
+  AddToTail(test, 8);
+  PrintList(test);
+  const unsigned k = 9;
+  ListNode* lastKNode = LastKNode(test, k);
+  if (lastKNode) {
+    std::cout << lastKNode->m_nKey << std::endl;
+  } else {
+    std::cout << "InputError" << std::endl;
+  }
+
+  return 0;
+}
